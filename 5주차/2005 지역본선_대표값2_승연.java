8681e775a5f80f3cba8381f3b67754a05e3a7e25
yeonyeonKim <46700771+yeonyeonKim@users.noreply.github.com>
2020-08-14 09:18:55 +0900

    Create 2005 지역본선_대표값2_승연.java

---
+++ 5주차/2005 지역본선_대표값2_승연.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i=0;i<5;i++){
+            arr[i] = sc.nextInt();
+            sum+=arr[i];
+        }
+        int avg = sum/5;
+        Arrays.sort(arr);
+        int mid = arr[2];
+        System.out.println(avg);
+        System.out.println(mid);
+    }
+}
