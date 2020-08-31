b10f01bd6487e9fcfd1b244caef431f43206df6c
yeonyeonKim <46700771+yeonyeonKim@users.noreply.github.com>
2020-08-31 22:57:29 +0900

    Create 2748_피보나치수2_승연.java

---
+++ 8주차/DP/2748_피보나치수2_승연.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[] dp = new long[91];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=90;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[N]);
+    }
+}
